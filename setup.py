bc2e2c1c2b292406d53f6900fb56d29ee7f53094
Alejandro Gutiérrez <npdsolutions@gmail.com>
2023-12-22 22:49:32 +0100

    update pypi readme

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
-import os
-import setuptools
+from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
-    README = readme.read()
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="HSPiPy",
-    version="0.0.1",
+    version="0.0.2",
     author="Alejandro Gutierrez",
     author_email="agutierrez@g-npd.com",
     description="Hansen Solubility Parameters in Python",
-    url="https://github.com/Gnpd/HSPy",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+    url="https://github.com/Gnpd/HSPiPy",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
